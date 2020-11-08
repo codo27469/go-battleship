1f01b10998223ae91085e56569613b579466bb07
Cody Borst <codo24769@gmail.com>
2020-11-07 23:19:34 -0800

    placing ships on the board

--- battleship.go
+++ battleship.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/jedib0t/go-pretty/table"
 )
@@ -17,29 +20,85 @@ func main() {
 	flag.Parse()
 
 	board := InitializeBoard(*width, *height)
+	s1, _ := CreateShip("A1", "A5")
+	s2, _ := CreateShip("B3", "B5")
+	s3, _ := CreateShip("D9", "J9")
+	PlaceShip(s1, board)
+	PlaceShip(s2, board)
+	PlaceShip(s3, board)
 	PrintBoard(board)
 }
 
-func InitializeBoard(width int, height int) [][]string {
+type BoardPiece struct {
+	Text    string
+	HasShip bool
+}
+
+type Ship struct {
+	Col1 int
+	Col2 int
+	Row1 int
+	Row2 int
+	Name string
+}
+
+func CreateShip(p1 string, p2 string) (Ship, error) {
+	if p1[0] != p2[0] && p1[1] != p2[1] {
+		return Ship{}, errors.New("invalid points for ship placement")
+	}
+	col1 := int(p1[0]) - 65
+	col2 := int(p2[0]) - 65
+	row1, _ := strconv.Atoi(string(p1[1]))
+	row2, _ := strconv.Atoi(string(p2[1]))
+	row1 -= 1
+	row2 -= 1
+	ship := Ship{col1, col2, row1, row2, fmt.Sprintf("%v-%v", p1, p2)}
+	return ship, nil
+}
+
+func InitializeBoard(width int, height int) [][]BoardPiece {
 	log.Printf("build board with dimensions %v x %v\n", width, height)
-	board := make([][]string, width)
+	board := make([][]BoardPiece, height)
 	for i := range board {
-		board[i] = make([]string, height)
+		board[i] = make([]BoardPiece, width)
 		for j := range board[i] {
-			board[i][j] = "?"
+			board[i][j] = BoardPiece{"?", false}
 		}
 	}
 	return board
 }
 
-func PrintBoard(board [][]string) {
+func PlaceShip(ship Ship, board [][]BoardPiece) ([][]BoardPiece, error) {
+	if ship.Col1 == ship.Col2 {
+		// ship is placed vertically
+		for i := range board {
+			for j := range board[i] {
+				if j == ship.Col1 && (i >= ship.Row1 && i <= ship.Row2) {
+					board[i][j].HasShip = true
+				}
+			}
+		}
+	} else if ship.Row2 == ship.Row2 {
+		// ship is place horizontally
+		for i := range board {
+			for j := range board[i] {
+				if i == ship.Row1 && (j >= ship.Col1 && j <= ship.Col2) {
+					board[i][j].HasShip = true
+				}
+			}
+		}
+	}
+	return board, nil
+}
+
+func PrintBoard(board [][]BoardPiece) {
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
 	t.SetAutoIndex(true)
 	for i := range board {
 		row := table.Row{}
 		for j := range board[i] {
-			row = append(row, board[i][j])
+			row = append(row, board[i][j].Text)
 		}
 		t.AppendRow(row)
 	}
